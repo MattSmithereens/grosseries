fed1ade81ce4efcbc2bec9a2dc0bd2e4367df3f4
Annie Shin and Matt Smith <matt@>
2018-06-04 15:08:06 -0700

    put user input into an array

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,39 @@
+//  var list = []
+//
+// $(document).ready(function() {
+//   $("#formOne").submit(function(event) {
+//     var userInput = "";
+//     var blanks = ["input#thing1"];
+//     blanks.forEach(function(blank) {
+//       userInput = $("input#" + blank).val();
+//       $("#grocout").append.list[""]
+//       console.log(userInput);
+//     });
+//
+//
+//
+//     $("#grocout").show();
+//     $("#grocin").hide();
+//     event.preventDefault();
+//   });
+// });
+var list = [];
+
 $(document).ready(function() {
-  $("#formOne").submit(function(event) {
+  $("#formOne").submit(function() {
     var userInput = "";
-    var blanks = ["thing1"];
+    var blanks = ["itemInput"];
+
+
     blanks.forEach(function(blank) {
       userInput = $("input#" + blank).val();
-      $("#groceries").append("<li>" + userInput + "</li>");
-      console.log(userInput);
+      console.log("Made it here : " + userInput);
+      list.push(userInput);
+      //$("#grocout").text(userInput);
+      console.log(list);
     });
 
-
-    // $("#formOne").click(function() {
-    //   $("ul#formOne").append("<li>")
-    // }
-
-    $("#groceries").show();
+    $("#grocout").show();
 
     event.preventDefault();
   });
